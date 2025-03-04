104f5553d4a6dfeba87a637ceee11f29214db2dd
矢野千紘 <yanonay3@gmail.com>
2025-03-05 01:31:18 +0900

    config修正

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  // GitHub Actionsからの環境変数GITHUB_REPOSITORYを取得
+  // 形式: {owner}/{repo}
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   output: 'export',
-  // リポジトリ名を指定（例：portfolio）
-  // ユーザー名.github.ioリポジトリの場合は空文字列にする
-  basePath: '', // リポジトリ名に置き換える
-  assetPrefix: '', // リポジトリ名に置き換える
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
